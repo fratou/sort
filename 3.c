ea8c60cfea920e8794ab2bc7c26afc198d2d4787
sort <2994527361@qq.com>
2018-03-26 23:23:37 +0800

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,16 @@
+void BubbleSort(int arr[],int n){
+     int i,j,tmp;
+     for(i=n-1;i>0;i--){
+          for(j=0;j<i;j++){
+             if(arr[j]<=arr[j-1]{
+               tmp =arr[j];
+               arr[j]=arr[j-1];
+               arr[j-1]=arr[j];
+              }
+           }
+     }
+}
+
+
+
+void SelectSort(int arr[],int n){
